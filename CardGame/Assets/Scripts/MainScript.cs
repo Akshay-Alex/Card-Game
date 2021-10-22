0257b4c6c92d4f45e5f161c47b3453be3c9ea1c8
Akshay Alex <akshayalex@qburst.com>
2021-10-23 02:07:58 +0530

    added menu and functionalities
    add, remove, reveal functionalities done.

--- CardGame/Assets/Scripts/MainScript.cs
+++ CardGame/Assets/Scripts/MainScript.cs
@@ -6,27 +6,46 @@ public class MainScript : MonoBehaviour
 {
 
     public GameObject CardPrefab;
-    public GameObject CurrentCard;
+    public GameObject CurrentCard,CurrentCardFrontObject;
     public Card CurrentCardScript;
+    public Renderer CurrentCardRenderer;
+    public Material CardMaterial;
+    
     int CardID = 0;
+    int[] AllowedCardNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+    int CurrentCardNumber = 0;
     float y=0;
+    Vector3 FaceDown = new Vector3(0, 0, 180);
+    Vector3 FaceUp = new Vector3(0, 0, 0);
     Stack<GameObject> deck = new Stack<GameObject>();
     // Start is called before the first frame update
 
-    void NewCard()
+    void AddCard(int CardNumber)
     {
         if (deck.Count < 13)
-        { 
-            CurrentCard = (GameObject) Instantiate(CardPrefab, new Vector3(0, y, 0), Quaternion.identity);
+        {
+            CurrentCard = (GameObject) Instantiate(CardPrefab, new Vector3(0, y, 0), Quaternion.Euler(FaceDown));
             y = (float) (y + 0.01);
             CurrentCard.gameObject.name = "Card" + CardID;
             CurrentCardScript = CurrentCard.GetComponent<Card>();
-            CurrentCardScript.CardNumber = CardID;
+            CurrentCardScript.CardNumber = CardNumber;
+            CurrentCardFrontObject = CurrentCard.transform.Find("front").gameObject;
+            CurrentCardRenderer = CurrentCardFrontObject.GetComponent<MeshRenderer>();
+            CardMaterial = Resources.Load("Materials/" + CardNumber, typeof(Material)) as Material;
+            CurrentCardRenderer.material = CardMaterial;
+            //mat[0] = Resources.Load("Materials/" + CardNumber, typeof(Material)) as Material;
+            //CurrentCardFrontObject.GetComponent<MeshRenderer>().materials[0] = Resources.Load("Material/"+ CardNumber + ".mat", typeof(Material)) as Material;
             CardID++;
             deck.Push(CurrentCard);
         }
     }
-    void RemoveCard()
+    public void NewCard()
+    {
+        CurrentCardNumber = Random.Range(1, 14);
+        AddCard(CurrentCardNumber);
+
+    }
+    public void RemoveCard()
     {
         if(deck.Count != 0)
         {
@@ -37,6 +56,11 @@ public class MainScript : MonoBehaviour
             CardID--;
         }
        
+    }
+    public void RevealCard()
+    {
+        var rot = Mathf.Lerp(180, 0, 1f);
+        CurrentCard.transform.rotation = Quaternion.Euler(0, 0, rot);
     }
     void Start()
     {
@@ -49,6 +73,7 @@ public class MainScript : MonoBehaviour
         if (Input.GetKeyDown("o"))
         {
             NewCard();
+
         }
         if (Input.GetKeyDown("l"))
         {
